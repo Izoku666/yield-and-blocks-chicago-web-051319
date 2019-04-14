378349caf9fcebb8af7d777ac84d2e00e3c8e224
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-14 02:15:45 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,14 +2,16 @@ def hello_t(x)
   
   index = 0
   while index < x.length
-  yield  x[index]
+    yield x[index]
    index += 1
   end
+  
   return x
 end
 
 # call your method here!
-hello_t(["Tim", "Tom", "Jim"]) do |name|
+v = ["Tim", "Tom", "Jim"]
+hello_t(v) do |name|
   if name.starts_with?("T")
     puts "Hi, #{name}"
   end
